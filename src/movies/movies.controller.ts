01acadd74b1dc0b6641d1f6d080b4777032d56a5
sh383 <ssh.daniel@hanmail.net>
2020-11-28 21:41:19 +0900

    #2.2 Movies Service part One

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,13 +1,45 @@
-import { Controller, Get, Param, Patch } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
-    @Get()
-    getAll() {
-       return 'hello' 
-    }
-    @Patch('/:id')
-    patch(@Param('id') movieId:string){
-        return `this will patch ${movieId}`
-    }
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: string) {
+    return this.moviesService.deleteOne(movieId);
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
+  }
 }
